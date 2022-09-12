4f5b84f0b67f9223634ee956d97a2608201b43b4
Dmitriy <104030338+iKHVRV@users.noreply.github.com>
2022-09-12 03:54:57 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const pressMe = document.getElementById('activate');
+const dr = document.getElementById('dr');
+const text = document.getElementById('text');
+const text2 = document.getElementById('text2');
+
+pressMe.addEventListener('click', () => setTimeout(hb, 1000));
+
+function hb() {
+  if ((pressMe.style.display = 'block')) {
+    pressMe.style.display = 'none';
+    dr.style.display = 'block';
+  }
+}
+
+let happy = setInterval(() => {
+  if ((text.style.display = 'block')) {
+    text.classList.toggle('active');
+    text2.classList.toggle('active2');
+    dr.classList.toggle('active3');
+  }
+}, 1500);
